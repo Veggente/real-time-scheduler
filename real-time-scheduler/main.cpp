5f7ce4eaeee4e26a5538d14d8881d2aee03dfc04
Veggente <veggente@gmail.com>
2013-10-28 17:48:26 -0700

    Test.

--- real-time-scheduler/main.cpp
+++ real-time-scheduler/main.cpp
@@ -2,7 +2,6 @@
 //  main.cpp
 //  real-time-scheduler
 //
-//  Created by Veggente on 10/28/13.
 //  Copyright (c) 2013 Xiaohan Kang. All rights reserved.
 //
 
@@ -10,7 +9,6 @@
 
 int main(int argc, const char * argv[])
 {
-
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;

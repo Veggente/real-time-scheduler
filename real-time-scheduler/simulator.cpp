167865b55ab3c5064be7f5131980f468793576b9
Veggente <veggente@gmail.com>
2013-12-04 17:48:42 -0700

    Added loop for ratio scan

--- real-time-scheduler/simulator.cpp
+++ real-time-scheduler/simulator.cpp
@@ -9,6 +9,8 @@
 #include <random>
 #include <iostream>  // NOLINT
 #include <fstream>  // NOLINT
+#include <sstream>
+#include <iomanip>
 #include <string>
 #include <vector>
 #include "./network_generator.h"
@@ -45,6 +47,8 @@ int simulator(const std::string &input_file) {
     int bandwidth_count;
     IntegerVector bandwidths;
     Ratios qos;
+    int ratio_count;
+    Ratios ratios;
     int num_iterations;
 
     // Read variables from input file
@@ -82,8 +86,10 @@ int simulator(const std::string &input_file) {
     } else if (arrival_dist_string == "binomial") {
         arrival_dist = BINOMIAL_PACKET;
         in >> binom_param;
+        std::ostringstream ostr;
+        ostr << binom_param;
         arrival_descriptor = "binomial("+std::to_string(max_packet)+", "
-            +std::to_string(binom_param)+")";
+            +ostr.str()+")";
     } else {
         std::cerr << "Error: Arrival distribution " << arrival_dist_string
             << " not recognized!" << std::endl;
@@ -108,64 +114,95 @@ int simulator(const std::string &input_file) {
         qos.push_back(temp);
     }
     in.ignore(std::numeric_limits<std::streamsize>::max(), ':');
+    in >> ratio_count;
+    in.ignore(std::numeric_limits<std::streamsize>::max(), ':');
+    for (int i = 0; i < ratio_count; ++i) {
+        double temp;
+        in >> temp;
+        ratios.push_back(temp);
+    }
+    in.ignore(std::numeric_limits<std::streamsize>::max(), ':');
     in >> num_iterations;
     in.close();
 
     // Initialize systems and names
     std::mt19937 rng;
-    std::vector<std::vector<QueueingSystem>> queueing_systems;
-    std::vector<std::vector<std::string>> queueing_system_names;
-        // per-policy, per-bandwidth systems
-    for (int i = 0; i < POLICY_COUNT; ++i) {
-        std::vector<QueueingSystem> temp_system_vector;
-        std::vector<std::string> temp_name_vector;
-        for (int j = 0; j < bandwidth_count; ++j) {
-            QueueingSystem temp_system(maximal_schedule_matrix,
-                                       static_cast<Policy>(i) , qos,
-                                       bandwidths[j], max_delay_bound);
-            temp_system_vector.push_back(temp_system);
-            std::string temp_name = network_type_string.substr(0, 2)
-                // TODO(Veggente): magic # 2
-                +std::to_string(network_size);
-            if ( (network_type == LINE) || (network_type == CYCLE) ) {
-                temp_name.append("-i"+std::to_string(interference_radius));
-            }
-            temp_name.append("-du"+std::to_string(max_delay_bound)+"-dl"
-                             +std::to_string(min_delay_bound)+"-it"
-                             +std::to_string(num_iterations)+"-"
-                             +policy_to_string(i)+"-b"
-                             +std::to_string(bandwidths[j])+".txt");
-            temp_name_vector.push_back(temp_name);
+    std::vector<std::vector<std::vector<QueueingSystem>>> queueing_systems;
+    std::vector<std::vector<std::vector<std::string>>> queueing_system_names;
+        // per-policy, per-bandwidth, per-qos-ratio systems
+    for (int policy_it = 0; policy_it < POLICY_COUNT; ++policy_it) {
+        std::vector<std::vector<QueueingSystem>> temp_system_matrix;
+        std::vector<std::vector<std::string>> temp_name_matrix;
+        for (int bandwidth_it = 0; bandwidth_it < bandwidth_count; ++bandwidth_it) {
+            std::vector<QueueingSystem> temp_system_vector;
+            std::vector<std::string> temp_name_vector;
+            for (int ratio_it = 0; ratio_it < ratio_count; ++ratio_it) {
+                Ratios scaled_qos = qos;
+                for (int k = 0; k < network_size; ++k) {
+                    scaled_qos[k] = scaled_qos[k]*ratios[ratio_it];
+                }
+                QueueingSystem temp_system(maximal_schedule_matrix,
+                                           static_cast<Policy>(policy_it) , scaled_qos,
+                                           bandwidths[bandwidth_it], max_delay_bound);
+                temp_system_vector.push_back(temp_system);
+                std::string temp_name = network_type_string.substr(0, 2)
+                    // TODO(Veggente): magic # 2
+                    +std::to_string(network_size);
+                if ( (network_type == LINE) || (network_type == CYCLE) ) {
+                    temp_name.append("-i"+std::to_string(interference_radius));
+                }
+//                temp_name.append("-du"+std::to_string(max_delay_bound)+"-dl"
+//                                 +std::to_string(min_delay_bound)+"-it"
+//                                 +std::to_string(num_iterations)+"-"
+//                                 +policy_to_string(i)+"-b"
+//                                 +std::to_string(bandwidths[j])+"-r"
+//                                 +std::to_string(ratios[ratio_it])+".txt");
+                std::ostringstream ostr;
+                ostr << ratios[ratio_it];
+                temp_name.append("-du"+std::to_string(max_delay_bound)+"-dl"
+                                 +std::to_string(min_delay_bound)+"-it"
+                                 +std::to_string(num_iterations)+"-"
+                                 +policy_to_string(policy_it)+"-b"
+                                 +std::to_string(bandwidths[bandwidth_it])+"-r"
+                                 +ostr.str()+".txt");
+                temp_name_vector.push_back(temp_name);
 
-            // Output network configuration to file
-            std::ofstream out(temp_name, std::ofstream::trunc);
-            if (!out) {
-                std::cerr << "Error: Could not open file " << temp_name << "!"
-                << std::endl;
-                exit(1);
-            }
-            out << "============Network Configuration============" << std::endl;
-            out << "Network type: " << network_type_string << std::endl;
-            out << "Network size: " << network_size << std::endl;
-            if ( (network_type == LINE) || (network_type == CYCLE) ) {
-                out << "Interference radius: " << interference_radius
+                // Output network configuration to file
+                std::ofstream out(temp_name, std::ofstream::trunc);
+                if (!out) {
+                    std::cerr << "Error: Could not open file " << temp_name
+                        << "!" << std::endl;
+                    exit(1);
+                }
+                out << "============Network Configuration============"
                     << std::endl;
+                out << "Network type: " << network_type_string << std::endl;
+                out << "Network size: " << network_size << std::endl;
+                if ( (network_type == LINE) || (network_type == CYCLE) ) {
+                    out << "Interference radius: " << interference_radius
+                        << std::endl;
+                }
+                out << "Policy: " << policy_to_string(policy_it) << std::endl;
+                out << "Max delay bound: " << max_delay_bound << std::endl;
+                out << "Min delay bound: " << min_delay_bound << std::endl;
+                out << "Bandwidth: " << bandwidths[bandwidth_it] << std::endl;
+                out << "Arrival distribution: " << arrival_descriptor
+                    << std::endl;
+                out << "Delay bound distribution: uniform(" << min_delay_bound
+                    << ", " << max_delay_bound << ")" << std::endl;
+                out << "Base QoS: " << qos << std::endl;
+                out << "QoS scalar: " << ratios[ratio_it] << std::endl;
+                out << "Number of iterations: " << num_iterations << std::endl;
+                out << std::endl;
+                out << "==================Deficits==================="
+                    << std::endl;
+                out.close();
             }
-            out << "Policy: " << policy_to_string(i) << std::endl;
-            out << "Max delay bound: " << max_delay_bound << std::endl;
-            out << "Min delay bound: " << min_delay_bound << std::endl;
-            out << "Bandwidth: " << bandwidths[j] << std::endl;
-            out << "Arrival distribution: " << arrival_descriptor << std::endl;
-            out << "Delay bound distribution: uniform(" << min_delay_bound
-                << ", " << max_delay_bound << ")" << std::endl;
-            out << "QoS: " << qos << std::endl;
-            out << "Number of iterations: " << num_iterations << std::endl;
-            out << std::endl;
-            out << "==================Deficits===================" << std::endl;
-            out.close();
+            temp_system_matrix.push_back(temp_system_vector);
+            temp_name_matrix.push_back(temp_name_vector);
         }
-        queueing_systems.push_back(temp_system_vector);
-        queueing_system_names.push_back(temp_name_vector);
+        queueing_systems.push_back(temp_system_matrix);
+        queueing_system_names.push_back(temp_name_matrix);
     }
 
     // Network evolution
@@ -186,11 +223,13 @@ int simulator(const std::string &input_file) {
         }
         for (int i = 0; i < POLICY_COUNT; ++i) {
             for (int j = 0; j < bandwidth_count; ++j) {
-                queueing_systems[i][j].arrive(traffic, rng);
-                queueing_systems[i][j].depart(rng);
-                queueing_systems[i][j].output_deficits(
-                    queueing_system_names[i][j]);
-                queueing_systems[i][j].clock_tick();
+                for (int ratio_it = 0; ratio_it < ratio_count; ++ratio_it) {
+                    queueing_systems[i][j][ratio_it].arrive(traffic, rng);
+                    queueing_systems[i][j][ratio_it].depart(rng);
+                    queueing_systems[i][j][ratio_it].output_deficits(
+                        queueing_system_names[i][j][ratio_it]);
+                    queueing_systems[i][j][ratio_it].clock_tick();
+                }
             }
         }
         progress_bar(time_slot, num_iterations);

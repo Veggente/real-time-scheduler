ccf2878490c14063e27a63e7179b0b0e2bed37d8
Veggente <veggente@gmail.com>
2014-01-08 17:04:27 -0700

    Modified Python scripts

---
+++ scripts/bandwidth.py
@@ -0,0 +1,75 @@
+#! /usr/bin/python
+def change_qos_ratio( new_ratio ):
+    import os
+    input_file = "input.txt"
+    temp_file = "input_temp.txt"
+    f = open(input_file)
+    new_file = open(temp_file, 'w')
+    for i, line in enumerate(f):
+        if i == 8:
+            new_line = line.split()
+            new_line[3] = repr(new_ratio)+"\n"
+            line = " ".join(new_line)
+        new_file.write(line)
+    f.close()
+    os.remove(input_file)
+    os.rename(temp_file, input_file)
+
+def run_real_time_scheduler():
+    import subprocess
+    args = "nice time ../real-time-scheduler input.txt network-5.txt stability".split()
+    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+    popen.wait()
+    output = popen.stdout.read()
+    print output
+
+def read_stability_ratio(input_file):
+    import os
+    f = open(input_file)
+    new_ratio = float(f.read().split()[-1])
+    f.close()
+    return new_ratio
+
+def word_reader(filename, row, column, num_indicator):
+    import os
+    f = open(filename)
+    return_string = "";
+    for i, line in enumerate(f):
+        if i == row-1:
+            return_string = line.split()[column-1]
+    f.close()
+    if num_indicator:
+        return float(return_string)
+    else:
+        return return_string
+
+def word_writer(filename, row, column, new_word):
+    import os
+    temp_filename = filename+"_temp"
+    f = open(filename)
+    new_file = open(temp_filename, 'w')
+    for i, line in enumerate(f):
+        if i == row-1:
+            new_line = line.split()
+            new_line[column-1] = str(new_word)
+            line = " ".join(new_line)
+            new_file.write(line)
+            new_file.write('\n')
+        else:
+            new_file.write(line)
+    f.close()
+    os.remove(filename)
+    os.rename(temp_filename, filename)
+
+def bisection_adjust(last_correct_value, this_value, step_size, min_value, max_value):
+    import math
+    import random
+    if math.fabs(last_correct_value-this_value) < step_size:
+        if random.random() > 0.5:
+            return min(this_value+step_size, max_value)
+        else:
+            return max(this_value-step_size, min_value)
+    elif last_correct_value > this_value:
+        return round((last_correct_value-this_value)/2.0/step_size)*step_size+this_value
+    else:
+        return this_value-round((this_value-last_correct_value)/2.0/step_size)*step_size
---
+++ scripts/find_max_qos_ratio.py
@@ -0,0 +1,51 @@
+#! /usr/bin/python
+import bandwidth
+import os
+
+# parameters to vary
+qos_ratio = 0.15 # initial ratio
+max_trial = 20 # maximum number of trials before terminating
+qos_step = 0.001 # step size
+policy_indicator = 1
+bw = 8 # bandwidth
+num_iterations = 1000 # number of iterations
+save_config_and_deficit = 0 # save data or not
+
+# parameters not likely to vary
+min_ratio = 0.0
+max_ratio = 1.0
+input_file = "input.txt"
+network_file = "network-5.txt"
+stability_file_prefix = "stability"
+
+# initialization
+bandwidth.word_writer(input_file, 11, 3, policy_indicator) # set policy indicator
+policy = ""
+if policy_indicator == 1:
+    policy = "ldf"
+elif policy_indicator == 2:
+    policy = "edf"
+elif policy_indicator == 4:
+    policy = "sdbf" # other policies are ignored
+bandwidth.word_writer(input_file, 7, 2, bw) # set bandwidth
+bandwidth.word_writer(input_file, 12, 3, 1) # set time seed
+bandwidth.word_writer(input_file, 13, 5, save_config_and_deficit)
+bandwidth.word_writer(input_file, 10, 4, num_iterations) # set number of iterations
+stability_file = stability_file_prefix+"-"+policy+"-b"+str(bw)+".txt"
+last_small = min_ratio
+last_large = max_ratio
+for trial in range(0, max_trial):
+    bandwidth.word_writer(input_file, 9, 4, qos_ratio) # set qos ratio
+    os.system("nice time ./real-time-scheduler "+input_file+" "+network_file+" "+stability_file_prefix)
+    # read new ratio
+    stability_ratio = bandwidth.read_stability_ratio(stability_file)
+        # if stable, go up
+    if stability_ratio < 1.5:
+        last_small = qos_ratio
+        qos_ratio = bandwidth.bisection_adjust(last_large, qos_ratio, qos_step, min_ratio, max_ratio)
+        # if not, go down
+    else:
+        last_large = qos_ratio
+        qos_ratio = bandwidth.bisection_adjust(last_small, qos_ratio, qos_step, min_ratio, max_ratio)
+    # if ratio has converged, stop
+    print "Iteration "+str(trial+1)+"/"+str(max_trial)+" completed!"

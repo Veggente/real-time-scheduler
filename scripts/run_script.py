7fe45835e4e45ed54b4fab5a5202cc44b80fbcad
Veggente <veggente@gmail.com>
2014-01-10 20:35:40 -0700

    Modified run_script.py to run all delays and qos's

--- scripts/run_script.py
+++ scripts/run_script.py
@@ -1,9 +1,20 @@
 #! /usr/bin/python
-import find_critical_qos
+import os
+import sys
 
 def main():
-    print "running find_critical_qos.py"
-    find_critical_qos.main()
+    qos_x = [0, 0.32, 0.73, 1, 1, 1]
+    qos_y = [1, 1, 1, 0.73, 0.32, 0]
+    qos_len = len(qos_x)
+    delays = [1, 2, 4, 8]
+    policy = sys.argv[1]
+    bw_begin = 24
+    bw_end = 72
+    bw_step = 24
+    for delay in delays:
+        for qos_it in range(qos_len):
+            os.system("./find_critical_qos.py -p "+policy+" -x "+str(qos_x[qos_it])+" -y "+str(qos_y[qos_it])+" -d "+str(delay)+" -b "+str(bw_begin)+" -e "+str(bw_end)+" -s "+str(bw_step))
+            print "Done with QoS ("+str(qos_x[qos_it])+", "+str(qos_y[qos_it])+"), delay", delay
 
 if __name__ == "__main__":
     main()
